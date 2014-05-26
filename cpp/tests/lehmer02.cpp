aba671bec129a7748cd8058b5c7ea5333643080f
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-05-25 20:45:09 -0400

    Updated lehmer02.cpp

--- cpp/tests/lehmer02.cpp
+++ cpp/tests/lehmer02.cpp
@@ -4,13 +4,12 @@ using namespace std;
 using namespace Groups;
 
 int main(void) {
- permutation_gen pg(5);
- permutation_gen::iterator iter;
- for (iter = pg.begin(); iter != pg.end(); iter++) {
-  int lehmer = (*iter).lehmer();
+ Permutation end = Permutation::firstOfDegree(6);
+ for (Permutation p = Permutation::identity(); p < end; p++)
+  int lehmer = p.lehmer();
   Permutation remhel = Permutation::fromLehmer(lehmer);
-  if (*iter != remhel)
-   cout << string(*iter) << " -> " << lehmer << " -> " << string(remhel) <<endl;
+  if (p != remhel)
+   cout << string(p) << " -> " << lehmer << " -> " << string(remhel) << endl;
  }
  return 0;
 }
