d809e01874a7cad468975ea3c6f4b3d63640221f
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-06-24 21:10:11 -0400

    Updated Tabular.cpp

--- cpp/Groups/Families/Tabular.hpp
+++ cpp/Groups/Families/Tabular.hpp
@@ -1,31 +1,52 @@
 #ifndef TABULAR_H
 #define TABULAR_H
 
+#include <map>
 #include "Groups/Group.hpp"
 
 namespace Groups {
- class Tabular : public Group {
+ class Tabular : public group<int> {
  public:
-  Tabular(const Group&);
+  template<class T> Tabular(const group<T>& g) {
+   std::vector<T> gelems = g.elements();
+   int qty = g.order();
+   std::map<T,int> elemdex;
+   for (int i=0; i<qty; i++) elemdex[gelems[i]] = i;
+   table = std::vector< std::vector<int> >(qty, std::vector<int>(qty));
+   for (int i=0; i<qty; i++) {
+    for (int j=0; j<qty; j++) {
+     table[i][j] = elemdex[g(gelems[i], gelems[j])];
+    }
+   }
+   inverses = std::vector<int>(qty);
+   orders = std::vector<int>(qty);
+   strs = std::vector<std::string>(qty);
+   for (int i=0; i<qty; i++) {
+    inverses[i] = elemdex[g.invert(gelems[i])];
+    orders[i] = g.order(gelems[i]);
+    strs[i] = g.showElem(gelems[i]);
+   }
+   abel = g.abelian();
+  }
+
   Tabular(const Tabular&);
   virtual ~Tabular() { }
-  virtual Element op(const Element&, const Element&) const;
-  virtual Element identity() const;
-  virtual std::vector<Element> elements() const;
-  virtual Element invert(const Element&) const;
+  virtual int operator()(const int&, const int&) const;
+  virtual int identity() const;
+  virtual std::vector<int> elements() const;
+  virtual int invert(const int&) const;
   virtual int order() const;
-  virtual int order(const Element&) const;
-  virtual std::string showElem(const Element&) const;
+  virtual int order(const int&) const;
+  virtual std::string showElem(const int&) const;
   virtual bool abelian() const;
   virtual Tabular* copy() const;
-  virtual int cmp(const Group*) const;
+  virtual int cmp(const group<int>*) const;
+  virtual bool contains(const int&) const;
  private:
   std::vector< std::vector<int> > table;
   std::vector<int> inverses, orders;
   std::vector<std::string> strs;
   bool abel;
-  typedef int elem_t;
-  typedef gelem<elem_t> element;
  };
 }
 
