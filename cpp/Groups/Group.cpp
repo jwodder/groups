5b8d5f46a59ba219c02106318e0d54a7dd55a9f4
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-07-13 19:00:17 -0400

    Put most of Group.hpp into a .cpp file, because apparently you can do that with template specializations

---
+++ cpp/Groups/Group.cpp
@@ -0,0 +1,108 @@
+#define GROUP_CHECKS_MEMBERSHIP
+
+#include <sstream>
+#include <string>
+#include <vector>
+#include "Groups/Group.hpp"
+using namespace std;
+
+namespace Groups {
+ int Group::oper(const int& x, const int& y) const {return table[x][y]; }
+
+ Element Group::oper(const Element& x, const Element& y) const {
+#ifdef GROUP_CHECKS_MEMBERSHIP
+  if (!contains(x) || !contains(y)) throw group_mismatch("Group::oper");
+#endif
+  return Element(this, table[x.val][y.val]);
+ }
+
+ Element Group::identity() const {return Element(this, 0); }
+
+ vector<Element> Group::elements() const {
+  vector<Element> elems(table.size(), identity());
+  vector<Element>::iterator iter = elems.begin();
+  int i=0;
+  for (iter++, i++; iter != elems.end(); iter++, i++) {
+   *iter = Element(this, i);
+  }
+  return elems;
+ }
+
+ int Group::invert(const int& x) const {return inverses[x]; }
+
+ Element Group::invert(const Element& x) const {
+#ifdef GROUP_CHECKS_MEMBERSHIP
+  if (!contains(x)) throw group_mismatch("Group::invert");
+#endif
+  return Element(this, inverses[x.val]);
+ }
+
+ int Group::order() const {return table.size(); }
+
+ int Group::order(const int& x) const {return orders[x]; }
+
+ int Group::order(const Element& x) const {
+#ifdef GROUP_CHECKS_MEMBERSHIP
+  if (!contains(x)) throw group_mismatch("Group::order");
+#endif
+  return orders[x.val];
+ }
+
+ string Group::showElem(const int& x) const {return strs[x]; }
+
+ string Group::showElem(const Element& x) const {
+#ifdef GROUP_CHECKS_MEMBERSHIP
+  if (!contains(x)) throw group_mismatch("Group::showElem");
+#endif
+  return strs[x.val];
+ }
+
+ bool Group::abelian() const {return abel; }
+
+ Group* Group::copy() const {return new Group(*this); }
+
+ int Group::cmp(const Group* other) const {
+  int ct = cmpTypes(*this, *other);
+  if (ct != 0) return ct;
+  const Group* c = static_cast<const Group*>(other);
+  return table < c->table ? -1 : table > c->table ? 1 : 0;
+  /* TODO: Should `strs` also take part in comparisons? */
+ }
+
+ bool Group::contains(const int& x) const {return 0 <= x && x < order(); }
+
+ bool Group::contains(const Element& x) const {
+  return x.gr->cmp(this) == 0 && 0 <= x.val && x.val < order();
+ }
+
+ int Group::indexElem(const Element& x) const {return x.index(); }
+
+ Group Group::direct(const Group& right) {
+  int leftQty = order(), rightQty = right.order();
+  int newQty = leftQty * rightQty;
+  vector< vector<int> > tbl(newQty, vector<int>(newQty));
+  for (int i=0; i<newQty; i++) {
+   int xa = i / rightQty, xb = i % rightQty;
+   for (int j=0; j<newQty; j++) {
+    int ya = j / rightQty, yb = j % rightQty;
+    tbl[i][j] = table[xa][ya] * rightQty + right.table[xb][yb];
+   }
+  }
+  vector<int> invs = vector<int>(newQty);
+  vector<int> ords = vector<int>(newQty);
+  vector<string> ss = vector<string>(newQty);
+  for (int i=0; i<newQty; i++) {
+   int a = i / rightQty, b = i % rightQty;
+   invs[i] = inverses[a] * rightQty + right.inverses[b];
+   ords[i] = lcm(orders[a], right.orders[b]);
+   if (i == 0) {
+    ss[i] = "1";
+   } else {
+    ostringstream out;
+    out << '(' << strs[a] << ", " << right.strs[b] << ')';
+    ss[i] = out.str();
+   }
+  }
+  return Group(tbl, invs, ords, ss, abel && right.abel);
+ }
+}
--- cpp/Groups/Group.hpp
+++ cpp/Groups/Group.hpp
@@ -4,10 +4,7 @@
 #ifndef GROUP_H
 #define GROUP_H
 
-#define GROUP_CHECKS_MEMBERSHIP
-
 #include <map>
-#include <sstream>
 #include <vector>
 #include "Groups/BasicGroup.hpp"
 #include "Groups/Element.hpp"
@@ -43,107 +40,24 @@ namespace Groups {
      abel(t.abel) { }
 
   virtual ~basic_group() { }
-
-  virtual int oper(const int& x, const int& y) const {return table[x][y]; }
-
-  virtual Element oper(const Element& x, const Element& y) const {
-#ifdef GROUP_CHECKS_MEMBERSHIP
-   if (!contains(x) || !contains(y)) throw group_mismatch("Group::oper");
-#endif
-   return Element(this, table[x.val][y.val]);
-  }
-
-  virtual Element identity() const {return Element(this, 0); }
-
-  virtual std::vector<Element> elements() const {
-   std::vector<Element> elems(table.size(), identity());
-   std::vector<Element>::iterator iter = elems.begin();
-   int i=0;
-   for (iter++, i++; iter != elems.end(); iter++, i++) {
-    *iter = Element(this, i);
-   }
-   return elems;
-  }
-
-  virtual int invert(const int& x) const {return inverses[x]; }
-
-  virtual Element invert(const Element& x) const {
-#ifdef GROUP_CHECKS_MEMBERSHIP
-   if (!contains(x)) throw group_mismatch("Group::invert");
-#endif
-   return Element(this, inverses[x.val]);
-  }
-
-  virtual int order() const {return table.size(); }
-
-  virtual int order(const int& x) const {return orders[x]; }
-
-  virtual int order(const Element& x) const {
-#ifdef GROUP_CHECKS_MEMBERSHIP
-   if (!contains(x)) throw group_mismatch("Group::order");
-#endif
-   return orders[x.val];
-  }
-
-  virtual std::string showElem(const int& x) const {return strs[x]; }
-
-  virtual std::string showElem(const Element& x) const {
-#ifdef GROUP_CHECKS_MEMBERSHIP
-   if (!contains(x)) throw group_mismatch("Group::showElem");
-#endif
-   return strs[x.val];
-  }
-
-  virtual bool abelian() const {return abel; }
-
-  virtual basic_group<Element>* copy() const {
-   return new basic_group<Element>(*this);
-  }
-
-  virtual int cmp(const basic_group<Element>* other) const {
-   int ct = cmpTypes(*this, *other);
-   if (ct != 0) return ct;
-   const basic_group<Element>* c = static_cast<const basic_group<Element>*>(other);
-   return table < c->table ? -1 : table > c->table ? 1 : 0;
-   /* TODO: Should `strs` also take part in comparisons? */
-  }
-
-  virtual bool contains(const int& x) const {return 0 <= x && x < order(); }
-
-  virtual bool contains(const Element& x) const {
-   return x.gr->cmp(this) == 0 && 0 <= x.val && x.val < order();
-  }
-
-  virtual int indexElem(const Element& x) const {return x.index(); }
-
-  basic_group<Element> direct(const basic_group<Element>& right) {
-   int leftQty = order(), rightQty = right.order();
-   int newQty = leftQty * rightQty;
-   std::vector< std::vector<int> > tbl(newQty, std::vector<int>(newQty));
-   for (int i=0; i<newQty; i++) {
-    int xa = i / rightQty, xb = i % rightQty;
-    for (int j=0; j<newQty; j++) {
-     int ya = j / rightQty, yb = j % rightQty;
-     tbl[i][j] = table[xa][ya] * rightQty + right.table[xb][yb];
-    }
-   }
-   std::vector<int> invs = std::vector<int>(newQty);
-   std::vector<int> ords = std::vector<int>(newQty);
-   std::vector<std::string> ss = std::vector<std::string>(newQty);
-   for (int i=0; i<newQty; i++) {
-    int a = i / rightQty, b = i % rightQty;
-    invs[i] = inverses[a] * rightQty + right.inverses[b];
-    ords[i] = lcm(orders[a], right.orders[b]);
-    if (i == 0) {
-     ss[i] = "1";
-    } else {
-     std::ostringstream out;
-     out << '(' << strs[a] << ", " << right.strs[b] << ')';
-     ss[i] = out.str();
-    }
-   }
-   return basic_group<Element>(tbl, invs, ords, ss, abel && right.abel);
-  }
+	  int oper(const int&, const int&) const;
+  virtual Element oper(const Element&, const Element&) const;
+  virtual Element identity() const;
+  virtual std::vector<Element> elements() const;
+	  int invert(const int&) const;
+  virtual Element invert(const Element&) const;
+  virtual int order() const;
+	  int order(const int&) const;
+  virtual int order(const Element&) const;
+	  std::string showElem(const int&) const;
+  virtual std::string showElem(const Element&) const;
+  virtual bool abelian() const;
+  virtual basic_group<Element>* copy() const;
+  virtual int cmp(const basic_group<Element>*) const;
+	  bool contains(const int&) const;
+  virtual bool contains(const Element&) const;
+  virtual int indexElem(const Element&) const;
+  basic_group<Element> direct(const basic_group<Element>&);
 
  private:
   basic_group(std::vector< std::vector<int> > tbl, std::vector<int> invs,
