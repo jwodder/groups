4aedbe3a6dbd5d3118a6a4b0ee20f6d9834ba721
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-07-25 19:46:05 -0400

    Finished first draft of group name parser

--- python2/dev/reader.py
+++ python2/dev/reader.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 
+__all__ = ["parse"]
+
+def readGroup(s): return GroupReader().read(s)
+
 class GroupReaderError(ValueError): pass
 
-tokens = {"Dih": ('Dih', r'\Dih', 'D'),
-	  "Dic": ('Dic', r'\Dic'),
+tokens = {"Dih": ('Dih', r'\Dih', r'\operatorname{Dih}', 'D'),
+	  "Dic": ('Dic', r'\Dic', r'\operatorname{Dic}'),
 	  "Z": ('Z', r'\Z', 'ℤ', 'C', r'\mathbb{Z}'),
-	  "A": ('A',),
 	  "S": ('S',),
+	  "A": ('A',),
 	  "Q": ('Q',),
 	  "V_4": ('V_4', 'V_{4}', 'V'),
 	  '×': ('times', r'\times', '×', 'x', 'X', '*'),
-	  '^×': ('^×', '^*', '^ˣ', r'^\rtimes', r'^\rtimes{}', r'^{\rtimes}', '^x', '^X'),
+	  '^×': ('^×', '^*', '^ˣ', '^x', '^X',
+		 r'^\rtimes', r'^\rtimes{}', r'^{\rtimes}', '^rtimes'),
 	  #'⋊': ('rtimes', r'\rtimes', 'x|'),
 	  '_': ('_',),
 	  '(': ('(',),
@@ -37,13 +42,13 @@ def lex(s):
 		stack.append(stack[-1][s[j]])
 		j += 1
 	    while stack and None not in stack[-1]:
-	        stack.pop()
+		stack.pop()
 		j -= 1
 	    if stack:
-	        yield stack[-1][None]
+		yield stack[-1][None]
 		i = j
 	    else:
-	        raise GroupReaderError('Invalid lexeme: %r' % (s[i:i+10],))
+		raise GroupReaderError('Invalid lexeme: %r' % (s[i:].split()[0],))
 	elif s[i].isdigit():
 	    j = i+1
 	    while j < len(s) and s[j].isdigit(): j += 1
@@ -58,4 +63,92 @@ def lex(s):
 	    yield int(s[i:j])
 	    i = j+1
 	else:
-	    raise GroupReaderError('Invalid lexeme: %r' % (s[i:i+10],))
+	    raise GroupReaderError('Invalid lexeme: %r' % (s[i:].split()[0],))
+
+def quat(n):
+    if n < 8:
+	raise GroupReaderError('Quaternion subscript must be at least 8')
+    i = 0
+    while n > 1:
+	n >>= 1
+	i += 1
+    if n /= 1:
+	raise GroupReaderError('Quaternion subscript must be a power of 2')
+    return Quaternion(i-1)
+
+subscripted = {"Dih": Dihedral,
+	       "Dic": Dicyclic,
+	       "Z":   Cyclic,
+	       "S":   Symmetric,
+	       "A":   Alternating,
+	       "Q":   quat}
+
+class GroupReader(object):
+    def __init__(self):
+	self.state = GroupReader.beforeGroup
+	self.stack = [[]]
+
+    def read(self, s):
+	for t in lex(s):
+	    self.state = self.state(t)
+	if self.state is not GroupReader.afterGroup:
+	    raise GroupReaderError('Input ended in middle of parse')
+	elif len(stack) > 1:
+	    raise GroupReaderError('Unclosed parentheses')
+	else:
+	    return stack[-1][-1]
+
+    def pushGroup(self, g):
+	if stack[-1]:
+	    stack[-1] = [DirectProduct(stack[-1][-1], g)]
+	else:
+	    stack[-1] = [g]
+
+    def beforeGroup(self, t):  # The next token sequence must be a group or (
+	if t == '(':
+	    stack.append([])
+	    return GroupReader.beforeGroup
+	elif t == 'V_4':
+	    self.pushGroup(Klein4())
+	    return GroupReader.afterGroup
+	elif t == 1:
+	    self.pushGroup(Trivial())
+	    return GroupReader.afterGroup
+	elif t in subscripted:
+	    self.classToken = t
+	    return GroupReader.expectUnderscore
+	else:
+	    raise GroupReaderError('Expected group, got %r' % (t,))
+
+    def expectUnderscore(self, t):
+	if t == '_':
+	    return GroupReader.expectSubscript
+	else:
+	    raise GroupReaderError('Underscore expected after %r' % (stack[-1][-1],))
+
+    def expectSubscript(self, t):
+	if isinstance(t, (int, long)):
+	    self.pushGroup(subscripted[self.classToken](t))
+	    if self.classToken == 'Z':
+		return GroupReader.afterCyclic
+	    else:
+		return GroupReader.afterGroup
+	else:
+	    raise GroupReaderError('Number expected after subscript')
+
+    def afterGroup(self, t):  # The next token sequence must be × or )
+	if t == '×':
+	    return Group.beforeGroup
+	elif t == ')':
+	    g = stack.pop()[0]
+	    if stack:
+		self.pushGroup(g)
+		return Group.afterGroup
+	    else:
+		raise GroupReaderError('Too many closing parentheses')
+
+    def afterCyclic(self, t):  # The next token sequence must be ^×, ×, or )
+	if t == '^×':
+	    stack[-1][-1] = AutCyclic(stack[-1][-1].n)
+	else:
+	    return self.afterGroup(t)
