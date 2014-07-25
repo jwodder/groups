69e51fa4e096525bdea13aba0eab5d299e20e545
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-07-24 20:47:39 -0400

    Added a function to Util.hpp for outputting Cayley tables of groups

--- cpp/Groups/Util.hpp
+++ cpp/Groups/Util.hpp
@@ -3,14 +3,49 @@
 #ifndef GROUPS_UTIL_H
 #define GROUPS_UTIL_H
 
+#include <ostream>
+#include <iomanip>  /* setw */
+#include <ios>  /* left */
 #include <stdexcept>  /* logic_error */
 #include <string>
+#include <vector>
+#include "BasicGroup.hpp"
 
 namespace Groups {
  class group_mismatch : public std::logic_error {
  public:
   explicit group_mismatch(const std::string& m) : logic_error(m) { }
  };
+
+ template<class T>
+ std::ostream& showTable(std::ostream& out, const basic_group<T>& g) {
+  std::vector<T> elems = g.elements();
+  typename std::vector<T>::const_iterator xiter, yiter;
+  size_t maxLen = 0;
+  for (xiter = elems.begin(); xiter != elems.end(); xiter++) {
+   size_t len = g.showElem(*xiter).size();
+   if (len > maxLen) maxLen = len;
+  }
+  out << std::left;
+  out << std::string(maxLen, ' ');
+  for (yiter = elems.begin(); yiter != elems.end(); yiter++) {
+   out << '|' << std::setw(maxLen) << *yiter;
+  }
+  out << std::endl;
+  std::string tmp = std::string(maxLen, '-');
+  out << tmp;
+  tmp = "|" + tmp;
+  for (int i=0; i<g.order(); i++) out << tmp;
+  out << std::endl;
+  for (xiter = elems.begin(); xiter != elems.end(); xiter++) {
+   out << std::setw(maxLen) << *xiter;
+   for (yiter = elems.begin(); yiter != elems.end(); yiter++) {
+    out << '|' << std::setw(maxLen) << g.showElem(g.oper(*xiter, *yiter));
+   }
+   out << std::endl;
+  }
+  return out;
+ }
 }
 
 #endif
--- cpp/tests/direct01.cpp
+++ cpp/tests/direct01.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <iomanip>  /* setw */
-#include <ios>  /* left */
-#include <string>
-#include <vector>
 #include "Groups/Families/Cyclic.hpp"
 #include "Groups/Families/Dicyclic.hpp"
 #include "Groups/Group.hpp"
+#include "Groups/Util.hpp"
 using namespace std;
 using namespace Groups;
 
@@ -14,30 +11,6 @@ int main() {
  Cyclic raw2(2);
  Group g1(raw1);
  Group g2(raw2);
- Group g = g1.direct(g2);
- vector<Element> elems = g.elements();
- vector<Element>::const_iterator xiter, yiter;
- size_t maxLen = 0;
- for (xiter = elems.begin(); xiter != elems.end(); xiter++) {
-  if (string(*xiter).size() > maxLen) maxLen = string(*xiter).size();
- }
- cout << left;
- cout << string(maxLen, ' ');
- for (yiter = elems.begin(); yiter != elems.end(); yiter++) {
-  cout << '|' << setw(maxLen) << *yiter;
- }
- cout << endl;
- string tmp = string(maxLen, '-');
- cout << tmp;
- tmp = "|" + tmp;
- for (int i=0; i<g.order(); i++) cout << tmp;
- cout << endl;
- for (xiter = elems.begin(); xiter != elems.end(); xiter++) {
-  cout << setw(maxLen) << *xiter;
-  for (yiter = elems.begin(); yiter != elems.end(); yiter++) {
-   cout << '|' << setw(maxLen) << (*xiter * *yiter);
-  }
-  cout << endl;
- }
+ showTable(cout, g1.direct(g2));
  return 0;
 }
