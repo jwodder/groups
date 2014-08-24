8324987da0a1bd098500dfb61b1f3cd6a0540a58
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-08-23 21:00:44 -0400

    Gave cayley.py an `-a` option for outputting ASCII tables instead of Unicode

--- python2/bin/cayley.py
+++ python2/bin/cayley.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python
 import sys
 sys.path.insert(1, sys.path[0] + '/..')
-from groups.read  import readName
+from groups.read import readName
+
+if len(sys.argv) < 2 or (sys.argv[1] == '-a' and len(sys.argv) == 2):
+    sys.stderr.write("Usage: %s [-a] group ...\n" % (sys.argv[0],))
+    sys.exit(2)
+
+if sys.argv[1] == '-a':
+    def showTbl(g): return g.cayley()
+    del sys.argv[1]
+else:
+    def showTbl(g): return g.cayleyU().encode('utf-8')
 
 for gname in sys.argv[1:]:
     g = readName(gname)
-    sys.stdout.write(g.cayleyU().encode('utf-8'))
-    #sys.stdout.write(g.cayley())
-    sys.stdout.write('\n')
+    sys.stdout.write(showTbl(g) + '\n')
