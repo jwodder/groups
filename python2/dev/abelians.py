f13ee959766dad911400fe1f898f28cc3a9b9897
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-11-29 20:13:07 -0500

    Arguably improved abelians.py

--- python2/dev/abelians.py
+++ python2/dev/abelians.py
@@ -1,20 +1,20 @@
-# This requires Python v.2.6+.
+import itertools
+from   operator import mul
+from   ???      import factor
 
 def abelians(n):
     """Returns an iterator over all abelian groups of order `n` paired with
        lists of their invariant factors"""
-    if n < 1: return
-    elif n == 1: yield (Trivial, [])
+    if n < 1:
+        return
+    elif n == 1:
+        yield (Trivial, [])
     else:
         vals = []
         for (p,k) in factor(n):
             vals.append([[p**i for i in part] for part in partitions(k)])
-        def mully(a,b):
-            if a is None: return b
-            elif b is None: return a
-            else: return a*b
         for xs in itertools.product(*vals):
-            xs2 = reduce(lambda a,b: map(mully, a, b), xs)
+            xs2 = reduce(lambda a,b: itertools.starmap(mul, izip_longest(a, b, fillvalue=1)), xs)
             yield (reduce(Direct, map(Cyclic, xs2)), xs2)
 
 def partitions(n):
@@ -22,8 +22,10 @@ def partitions(n):
         if qty == 0:
             yield ()
         else:
-            for i in range(min(qty,mx), 0, -1):
+            for i in xrange(min(qty,mx), 0, -1):
                 for xs in gen(qty-i, i):
                     yield (i,) + xs
-    if n < 1: raise ValueError
-    else: return gen(n,n)
+    if n < 1:
+        raise ValueError
+    else:
+        return gen(n,n)
